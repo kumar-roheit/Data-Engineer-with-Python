ede747a58ded8f04815bd2bf862f245a1f10d8e6
rohit <rohitzx@outlook.com>
2020-09-16 19:57:14 -0500

    added codes to read/write parquet files
    added codes for writing and reading from parquet files.

--- Extract, Transform and Load (ETL)/Miscellaneous.py
+++ Extract, Transform and Load (ETL)/Miscellaneous.py
@@ -1,6 +1,10 @@
-#  1. To get the data into the Spark framework.
+#  1.  To get the data into the Spark framework.
 spark.read.jdbc("jdbc:postgresql://localhost:5432/pagila",
                 "customer",
                 {"user":"repl","password":"password"})
                 
-                
+# 2.1. Write the pandas DataFrame film_pdf to a parquet file called "films_pdf.parquet".
+        film_pdf.to_parquet("films_pdf.parquet")
+
+# 2.2. Write the PySpark DataFrame film_sdf to a parquet file called "films_sdf.parquet".
+        film_sdf.write.parquet("films_sdf.parquet")
