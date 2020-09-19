12ce02dcc500d2fcfba1600fa72a8d5b2a773aa3
rohit <rohitzx@outlook.com>
2020-09-19 15:07:55 -0500

    Create importing_data_from_json.py

---
+++ Streamlined Data Ingestion with pandas/importing_data_from_json.py
@@ -0,0 +1,2 @@
+      JSON isn't a tabular format, so pandas makes assumptions about its orientation when loading data.
+      Load dhs_report_reformatted.json to a data frame with orient specified.
