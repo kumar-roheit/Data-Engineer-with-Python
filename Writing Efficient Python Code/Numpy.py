3f728106db717b8b137c500a85aa6f9682bdeca1
rohit <rohitzx@outlook.com>
2020-09-19 18:23:54 -0500

    initial commit

---
+++ Writing Efficient Python Code/Numpy.py
@@ -0,0 +1,7 @@
+# Numpy arrays are Homogeneous - they must contain elements of the same type 
+# if we create the element with mixed types like int and float ... numpy will convert the numpy arrays to float.
+
+# Numpy arrays broadcast 
+
+nums = np.array([1,2])
+nums ** 2
