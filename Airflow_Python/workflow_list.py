b4a71ce243a5469e8dd70d94e23ba07f1d95bdf9
rohit <rohitzx@outlook.com>
2020-10-11 19:40:46 -0500

    Create workflow_list.py

---
+++ Airflow_Python/workflow_list.py
@@ -0,0 +1,2 @@
+Question you need to run the command cleandata.sh with the date argument and the file argument as before, except now you have a list of 30 files. You do not want to create 30 tasks, so your job is to modify the code to support running the argument for 30 or more files.
+The Python list of files is already created for you, simply called filelist.
